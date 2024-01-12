99943da7e0aee9cae9576d429b21b7c0a269e7ef
Humphrey Yeboah <haky@haky.com>
2024-01-12 12:25:58 +0000

    feat: add fortify to app

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+// use App\Services\Auth\JwtGuard;
+// use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+// use Illuminate\Support\Facades\Gate;
+// use Illuminate\Support\Facades\Auth;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,5 +26,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        // Auth::extend('jwt', function (Application $app, string $name, array $config) {
+        //     // Return an instance of Illuminate\Contracts\Auth\Guard...
+
+        //     return new JwtGuard(Auth::createUserProvider($config['provider']));
+        // });
+
     }
 }
